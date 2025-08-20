cfbe7489fd97f1a2b3b31c2a4aeab3c7848843e9
[dyad] <youssef.sala7@gmail.com>
2025-08-20 04:55:55 +0300

    [dyad] Ensured global background and modal overlay apply by removing page override and adding overlay CSS; restarted app - wrote 2 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -63,7 +63,7 @@ const Index = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50 flex">
+    <div className="min-h-screen flex">
       <Sidebar currentPage={currentPage} onPageChange={setCurrentPage} />
       <div className="flex-1 overflow-auto">
         <main className="w-full">
